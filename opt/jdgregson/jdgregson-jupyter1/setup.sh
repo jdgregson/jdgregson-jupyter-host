1c1ad4706967b6b921c734202d77bc902e90a843
root@jdgregson-jupyter1.jdgregson.com <root@jdgregson-jupyter1.jdgregson.com>
2024-02-25 20:28:50 +0000

    Added setup.sh

---
+++ opt/jdgregson/jdgregson-jupyter1/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Setup script for jdgregson-jupyter1
+# Author: Jonathan Gregson <hello@jdgregson.com>
+
+# Install dependencies
+apt-get update
+apt-get install \
+	podman \
+	unattended-upgrades
+
