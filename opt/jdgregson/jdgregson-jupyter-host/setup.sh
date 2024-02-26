76706a3898ccd4209ba1303e3b3efb956af9d74f
root@jdgregson-jupyter1.jdgregson.com <root@jdgregson-jupyter1.jdgregson.com>
2024-02-26 03:48:28 +0000

    Updates

--- opt/jdgregson/jdgregson-jupyter-host/setup.sh
+++ opt/jdgregson/jdgregson-jupyter-host/setup.sh
@@ -8,26 +8,28 @@ if [ ! -f "/etc/lsb-release" ] || [ -z "grep '22.04' /etc/lsb-release" ]; then
     exit 1
 fi
 
-# Install dependencies
+echo "Installing updates and dependencies..."
 apt-get update
+apt-get upgrade
 apt-get install \
     podman \
     unattended-upgrades
 
-# Create user to run the container
 USER="jupyteruser"
 if [ ! -d "/home/$USER" ]; then
+    echo "Creating user $USER..."
     useradd -m "$USER"
 fi
 
-# Pull in jdgregson-jupyter-host
+echo "Deploying jdgregson-jupyter-host..."
 DEPLOY_DIR=$(mktemp -d)
 git clone https://github.com/jdgregson/jdgregson-jupyter-host.git "$DEPLOY_DIR/"
+cd "$DEPLOY_DIR"
 "$DEPLOY_DIR/opt/jdgregson/jdgregson-jupyter-host/restore-permissions.sh" "$DEPLOY_DIR"
 rsync -pvr "$DEPLOY_DIR/" /
 rm -fr "$DEPLOY_DIR"
 
-# Pull in jdgregson-jupyter
+echo "Deploying jdgregson-jupyter..."
 git clone https://github.com/jdgregson/jdgregson-jupyter.git /opt/jdgregson/jdgregson-jupyter
 chmod +x /opt/jdgregson/jdgregson-jupyter/*.sh
 
