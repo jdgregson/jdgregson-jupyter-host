722db5a5edd0c07fecc4e6507fd49d752a4c5243
root@jdgregson-jupyter1.jdgregson.com <root@jdgregson-jupyter1.jdgregson.com>
2024-02-26 02:22:56 +0000

    Updates

--- opt/jdgregson/jdgregson-jupyter-host/setup.sh
+++ opt/jdgregson/jdgregson-jupyter-host/setup.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
-# Setup script for jdgregson-jupyter1
+# Setup script for jdgregson-jupyter-host
 # Author: Jonathan Gregson <hello@jdgregson.com>
+
+DEPLOYMENT_ID=$(head /dev/random -c 32 | md5sum)
 
 # Install dependencies
 apt-get update
 apt-get install \
 	podman \
 	unattended-upgrades
+
+# Create user to run the container
+USER="jupyteruser"
+if [ ! -d "/home/$USER" ]; then
+	useradd -m "$USER"
+fi
+
+# Pull in jdgregson-jupyter-host
+DEPLOY_DIR="/tmp/jdgregson-jupyter-host-$DEPLOYMENT_ID"
+mkdir "$DEPLOY_DIR"
+git clone https://github.com/jdgregson/jdgregson-jupyter-host.git "$DEPLOY_DIR/"
+
+
+
+#rsync -pvr "$DEPLOY_DIR/" /
+#rm -fr "$DEPLOY_DIR"
+
+# Pull in jdgregson-jupyter
+mkdir /opt/jdgregson
+git clone https://github.com/jdgregson/jdgregson-jupyter.git /opt/jdgregson/jdgregson-jupyter
+chmod +x /opt/jdgregson/jdgregson-jupyter/*.sh
 
