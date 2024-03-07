e538bb36d003c04bf47e8a66e1ba86e70550d3d8
root@jdgregson-jupyter1.jdgregson.com <root@jdgregson-jupyter1.jdgregson.com>
2024-03-07 07:30:24 +0000

    It works, ship it!

--- README.md
+++ README.md
@@ -1,22 +1,18 @@
 # jdgregson-jupyter-host
 
-This repository contains the host configuration of hosts powering jupyter.jdgregson.com.
+This repository contains the host configuration for hosts powering jupyter.jdgregson.com.
 
 ## Deployment
 
-1. Create host secrets file: `vi /root/host-secrets`:
+1. Create secrets file: `vi /root/secrets`:
 
 ```
 export CLOUDFLARED_TOKEN=...
-```
-
-2. Create host secrets file: `vi /root/host-secrets`:
-
-```
 export HF_TOKEN=...
+export JUPYTER_ACCESS_TOKEN=...
 ```
 
-3. Deploy:
+2. Deploy:
 
 ```
 curl https://raw.githubusercontent.com/jdgregson/jdgregson-jupyter-host/main/opt/jdgregson/jdgregson-jupyter-host/setup.sh | sudo bash
--- opt/jdgregson/jdgregson-jupyter-host/setup.sh
+++ opt/jdgregson/jdgregson-jupyter-host/setup.sh
@@ -3,32 +3,31 @@
 # Setup script for jdgregson-jupyter-host
 # Author: Jonathan Gregson <hello@jdgregson.com>
 
-if [ ! -f "/etc/lsb-release" ] || [ -z "grep '22.04' /etc/lsb-release" ]; then
-    echo "ERROR: jdgregson-jupyter-host only supports Ubuntu Server 22.04."
+USER="jupyteruser"
+IP=0.0.0.0
+PORT=8888
+
+if [ ! -f "/etc/lsb-release" ] || [ -z "$(grep '22.04' /etc/lsb-release)" ]; then
+    echo "ERROR: jdgregson-jupyter-host only supports Ubuntu Server 22.04"
     exit 1
 fi
 
-if [ ! -f "/root/host-secrets" ]; then
-    echo "ERROR: /root/host-secrets not found!"
-    exit 1
-fi
-if [ ! -f "/root/container-secrets" ]; then
-    echo "ERROR: /root/container-secrets not found!"
+if [ ! -f "/root/secrets" ]; then
+    echo "ERROR: /root/secrets not found"
     exit 1
 fi
 
-source /root/host-secrets
+source /root/secrets
 cd ~
 
 echo "Installing updates and dependencies..."
 apt-get update
 NEEDRESTART_MODE=a apt-get upgrade --yes
 NEEDRESTART_MODE=a apt-get install --yes \
-    podman \
+    python3-pip \
     unattended-upgrades
 
 echo "Creating and configuring user..."
-USER="jupyteruser"
 if [ ! -d "/home/$USER" ]; then
     echo "Creating user $USER..."
     useradd -m "$USER"
@@ -37,9 +36,12 @@ if [ ! -d "/home/$USER/notebooks" ]; then
     mkdir "/home/$USER/notebooks"
     chown $USER:$USER "/home/$USER/notebooks"
 fi
-cp /root/container-secrets "/home/$USER/"
-chown $USER:$USER "/home/$USER/container-secrets"
 
+echo "Deploying cloudflared..."
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+dpkg -i cloudflared.deb
+cloudflared service install $CLOUDFLARED_TOKEN
+rm cloudflared.deb
 
 echo "Deploying jdgregson-jupyter-host..."
 DEPLOY_DIR=$(mktemp -d)
@@ -48,18 +50,29 @@ git clone https://github.com/jdgregson/jdgregson-jupyter-host.git "$DEPLOY_DIR/"
 cp -fr "$DEPLOY_DIR"/* /
 rm -fr "$DEPLOY_DIR"
 
-echo "Deploying jdgregson-jupyter..."
-git clone https://github.com/jdgregson/jdgregson-jupyter.git /opt/jdgregson/jdgregson-jupyter
-chmod +x /opt/jdgregson/jdgregson-jupyter/*.sh
+echo "Installing Jupyter and plugins..."
+sudo pip3 install jupyterlab jupyter-resource-usage jupyterlab_theme_solarized_dark
 
-echo "Deploying cloudflared"
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
-dpkg -i cloudflared.deb
-echo "Cloudflared token: $CLOUDFLARED_TOKEN"
-cloudflared service install $CLOUDFLARED_TOKEN
-rm cloudflared.deb
+echo "Creating Jupyter service..."
+cat >/etc/systemd/system/jupyter.service <<EOF
+[Unit]
+Description=Jupyter
 
-echo "Enabing and starting jdgregson-jupyter service..."
-systemctl enable jdgregson-jupyter
-systemctl start jdgregson-jupyter
+[Service]
+Type=simple
+ExecStart=/usr/bin/env jupyter lab --ip=$IP --port=$PORT --LabApp.token='$JUPYTER_ACCESS_TOKEN'
+
+WorkingDirectory=/home/$USER/notebooks
+User=$USER
+Group=$USER
+
+Restart=on-failure
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "Starting Jupyter on $IP:$PORT..."
+systemctl enable --now jupyter
 
